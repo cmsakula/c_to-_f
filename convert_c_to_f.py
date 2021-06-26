406e74ee2032e20cf728f23c0050b9ef01854050
Eric Tang <etang619@gmail.com>
2021-06-26 12:23:23 +0800

    add new file

---
+++ convert_c_to_f.py
@@ -0,0 +1,27 @@
+# 隨意練習
+#y = input ('is raining today? ')
+#x = 5
+#z = 1.14241
+#w = False
+#print(w, x, y, z)
+
+#print(x==5)
+#print(x>5)
+#print(x<5)
+
+#if y == 'yes':
+#	print('stay home')
+#	print('dont go out')
+#else :
+#	print('go out and play')
+
+#age = input ('How old are you? ')
+#age = int(age)
+#if age > 20:
+#	print('You may vote')
+
+c_degree = input('enter Celsius degree:')
+c_degree = float(c_degree)
+f_degree = (c_degree * 9 / 5) + 32 
+print ('Fahrenheit is ', f_degree)
+
